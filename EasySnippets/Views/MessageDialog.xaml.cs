6ac0faa32339ada1de0f28abd38fe4393346ff0f
Karol Berezicki <karolberezicki@gmail.com>
2017-10-12 20:33:51 +0200

    Added autosave, added new message box

---
+++ EasySnippets/Views/MessageDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using MahApps.Metro.SimpleChildWindow;
+
+namespace EasySnippets.Views
+{
+    /// <summary>
+    /// Interaction logic for ClosingDialog.xaml
+    /// </summary>
+    public partial class MessageDialog : ChildWindow
+    {
+        public string MessageBoxText { get; set; }
+        public string Caption { get; set; }
+
+        public MessageDialog()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        //public MessageDialog(string messageBoxText, string caption)
+        //{
+        //    MessageBoxText = messageBoxText;
+        //    Caption = caption;
+        //}
+
+        private void DialogDismiss(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
